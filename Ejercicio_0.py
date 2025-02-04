ff2bdd002bbb4c47f6588615c9d89cd276f0e1bf
Diego Patzan <diegopatzan24@gmail.com>
2025-02-03 20:02:07 -0600

    Ejercicio_0 inciso a completado

--- Ejercicio_0.py
+++ Ejercicio_0.py
@@ -11,7 +11,21 @@ df = pd.read_csv('MM_Data.csv')
 total_muestras = len(df)
 muestras_R = len(df[df['Red'] >= 10])
 
+# b) Calcular P(T): probabilidad de tener al menos 57 botonetas en total
+# Sumar todas las columnas de colores para obtener el total de botonetas por muestra
+df['Total'] = df[['Red', 'Green', 'Blue', 'Orange', 'Yellow', 'Brown']].sum(axis=1)
+
+
+# Filtrar las muestras que cumplen con el evento T (al menos 57 botonetas en total)
+muestras_T = len(df[df['Total'] >= 57])
+
 # Calcular la probabilidad
 P_R = muestras_R / total_muestras
 
+P_T = muestras_T / total_muestras
+
+
+
 print(f"Probabilidad P(R) de tener al menos 10 botonetas rojas: {P_R:.4f}")
+
+print(f"Probabilidad P(T) de tener al menos 57 botonetas en total: {P_T:.4f}")
